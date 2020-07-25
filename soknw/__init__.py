25e0f9434452ec023baa24e37c36420de080d487
thebhumbak <skbhumbak@gmail.com>
2020-07-25 11:22:36 +0530

    import resolved

--- soknw/__init__.py
+++ soknw/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-from soknw import routes
+
 
 # TODOS
 #     -1. visual feedback of form validation
@@ -14,3 +14,5 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'red-text text-accent-1'
+# import routes from sonkw here(below everything else) to avoid complications of circular importing
+from soknw import routes
