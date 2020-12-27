dec5812293fe2b9724e522ecabbc4cb0ca8fa17a
thebhumbak <skbhumbak@gmail.com>
2020-12-27 17:37:23 +0530

    sat commit

--- soknw/routes.py
+++ soknw/routes.py
@@ -2,23 +2,51 @@ import os
 import string 
 import random 
 import bcrypt
-from flask import Flask, flash, redirect, render_template, request, url_for, send_file, send_from_directory, abort
-from flask_login import current_user, login_required, login_user, logout_user
 from PIL import Image
 from soknw import app, db
-from soknw.forms import LoginForm, RegistrationForm, UpdateAccountForm
 from soknw.models import Book, User
+from soknw.forms import (LoginForm, 
+                        RegistrationForm,
+                        UpdateAccountForm)
+from flask_login import (current_user,
+                        login_required,
+                        login_user, logout_user)
+from flask import (Flask, flash, redirect,
+                    render_template, request, 
+                    url_for, send_file, 
+                    send_from_directory, abort)
 
-#app.config['allbooks']="/mnt/d/Web/Flask projects/So_know/soknw/static/books/PDFs"
-
-app.config['allbooks']="/app/soknw/static/books/PDFs"
+app.config['allbooks']="/mnt/d/Web/Flask projects/So_know/soknw/static/books/PDFs"
+# app.config['allbooks']="/app/soknw/static/books/PDFs"
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    
-    return render_template('home.html',title='book')
+    return render_template('home.html',title='Home')
+
+@app.route('/library')
+def library():
+    books = Book.query.all()
+    return render_template('library.html', books=books, title='Library')
+
+@app.route("/library/<int:book_id>/")
+def book(book_id):
+    book = Book.query.get(book_id)
+    book_title = book.title
+    book_id = book_id
+    return render_template('book.html', title='book', book_title =book_title, book_id = book_id)
+
+@app.route("/library/get/<int:book_id>/")
+def getbook(book_id):
+    bookname = Book.query.get(book_id).title
+
+    try:
+        return send_from_directory(
+            app.config["allbooks"], filename=bookname, as_attachment=False
+            )
+    except FileNotFoundError:
+        abort(404)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -54,18 +82,34 @@ def login():
             flash('Login Unsuccessful. Please check email and password', 'red accent-1' )
     return render_template('login.html', title='Login', form=form)
 
-
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/library')
-def library():
-    books = Book.query.all()
-    return render_template('library.html', books=books, title='Library')
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form =UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.avtar.data:
+            avtar_file = save_avtar(form.avtar.data)
+            current_user.avtar = avtar_file
+        current_user.name = form.name.data
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated !', 'green accent-1')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.name.data = current_user.name
+        form.username.data = current_user.username
+        form.email.data = current_user.email
 
+    avtar = url_for('static', filename = f'images/Profile_pictures/{ current_user.avtar }')
+    # { current_user.avtar }
+    return render_template('account.html', title='Account', avtar=avtar, form=form)
 
 def save_avtar(avtar):
 
@@ -93,56 +137,7 @@ def save_avtar(avtar):
     avtar.save(large_image_path) # SAVING THE original FILE IN FOLDER
     return image_flnm #RETURNING NAME OF FILE TO UPDATE IN DATABASE
 
-@app.route('/account', methods=['GET', 'POST'])
-@login_required
-def account():
-    form =UpdateAccountForm()
-    if form.validate_on_submit():
-        if form.avtar.data:
-            avtar_file = save_avtar(form.avtar.data)
-            current_user.avtar = avtar_file
-        current_user.name = form.name.data
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your account has been updated !', 'green accent-1')
-        return redirect(url_for('account'))
-    elif request.method == 'GET':
-        form.name.data = current_user.name
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-
-    avtar = url_for('static', filename = f'images/Profile_pictures/{ current_user.avtar }')
-    # { current_user.avtar }
-    return render_template('account.html', title='Account', avtar=avtar, form=form)
-
-
-@app.route("/library/<int:book_id>/")
-def book(book_id):
-    book = Book.query.get(book_id)
-    book_title = book.title
-    book_id = book_id
-    return render_template('book.html', title='book', book_title =book_title, book_id = book_id)
-
-
-
-
-
-@app.route("/library/get/<int:book_id>/")
-def getbook(book_id):
-    bookname = Book.query.get(book_id).title
-
-    try:
-        return send_from_directory(
-            app.config["allbooks"], filename=bookname, as_attachment=False
-            )
-    except FileNotFoundError:
-        abort(404)
-        
-
-
 # routefor favicon
-
 @app.route("/favicon.ico")
 def favicon():
 	return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsof.icon')
