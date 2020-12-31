78bffb8ae89da75216b96700036e1705b8387d36
thebhumbak <skbhumbak@gmail.com>
2020-12-31 16:48:09 +0530

    minor

--- soknw/models.py
+++ soknw/models.py
@@ -27,7 +27,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
-    cover = db.Column(db.String(20), nullable=False, default=f'defaul.jpg')
+    cover = db.Column(db.String(100), nullable=False, default=f'default.jpg')
     date_posted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
     discription = db.Column(db.Text, nullable=True)
     # reader = db.relationship("User", secondary='Reads', backref=db.backref('reader', lazy = 'dynamic'))
